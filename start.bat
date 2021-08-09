f9a60f0178a81c520496d6e0e60f4a0e31cfcd8d
ItsNotAlan <48403150+ItsNeverAlan@users.noreply.github.com>
2021-08-09 20:43:06 +0800

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,9 +1,7 @@
 @echo off
 del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk" > out.txt 2>&1
-net config server /srvcomment:"Windows Server 2019 By Oshekher" > out.txt 2>&1
-REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F > out.txt 2>&1
-REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /f /v Wallpaper /t REG_SZ /d D:\a\wallpaper.bat
-net user administrator @Oshekher /add >nul
+net config server /srvcomment:"Windows Server 2019 By HaeImAlan" > out.txt 2>&1
+net user administrator A123456@ /add >nul
 net localgroup administrators administrator /add >nul
 net user administrator /active:yes >nul
 net user installer /delete
@@ -12,10 +10,10 @@ sc config Audiosrv start= auto >nul
 sc start audiosrv >nul
 ICACLS C:\Windows\Temp /grant administrator:F >nul
 ICACLS C:\Windows\installer /grant administrator:F >nul
-echo Successfully Installed !, If the RDP is Dead, Please Rebuild Again!
+echo Success! Please rebuild if the rdp doesnt seem to be available.
 echo IP:
 tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Tidak bisa mendapatkan NGROK tunnel, pastikan NGROK_AUTH_TOKEN benar di Settings> Secrets> Repository secret. Mungkin VM Anda sebelumnya masih berjalan: https://dashboard.ngrok.com/status/tunnels "
 echo Username: administrator
-echo Password: @Oshekher
-echo Silahkan Login Ke RDP Anda!!
+echo Password: A123456@
+echo Log into rdp with the provided credentials
 ping -n 10 127.0.0.1 >nul
